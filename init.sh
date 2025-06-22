e8cafa654f3afdc6c78072e89acd819b44d65f56
Morteza PRK <mortezaPRK@users.noreply.github.com>
2025-06-22 19:38:03 +0200

    no more ydotool :)

--- init.sh
+++ init.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
+wget https://github.com/mortezaPRK/cec-keyboard/releases/download/v0.1.0/cec-keyboard_linux_arm64
+mv cec-keyboard_linux_arm64 cec-keyboard
 sudo sed -i 's/dtoverlay=vc4.*/dtoverlay=vc4-kms-v3d-pi4,cma-512,nohdmi1/g' /boot/firmware/config.txt && \
 sudo sed -i 's/camera_auto_detect=1/dtoverlay=disable-bt/g' /boot/firmware/config.txt && \
 sudo apt update && \
 sudo apt dist-upgrade -y && \
 sudo apt install -y \
-    cage \
     cec-utils \
     chromium-browser \
     gstreamer1.0-libav \
     gstreamer1.0-plugins-bad \
     gstreamer1.0-plugins-base \
     gstreamer1.0-plugins-good \
-    pulseaudio \
-    pulseaudio-utils \
     rpi-chromium-mods \
     seatd \
     vim \
-    uxplay \
-    xwayland && \
-sudo raspi-config && \
+    uxplay && \
 sudo mv /home/pi/backup/pi /etc/pam.d/pi && \
-sudo mv /home/pi/backup/cage@.service /home/pi/backup/uxplay.service /home/pi/backup/cec-translate.service /home/pi/backup/ydotoold.service /etc/systemd/system/ && \
-sudo mv /home/pi/backup/cec-translate /home/pi/backup/ydotoold /home/pi/backup/ydotool /home/pi/backup/chromium_launcher /home/pi/backup/cog_launcher /usr/local/bin/ && \
+sudo mv /home/pi/backup/cage@.service /home/pi/backup/uxplay.service /home/pi/backup/cec-keyboard.service /etc/systemd/system/ && \
+sudo mv /home/pi/cec-keyboard /home/pi/backup/chromium_launcher /usr/local/bin/ && \
+sudo mv /home/pi/backup/cec-keyboard.yml /etc/
 sudo systemctl daemon-reload && \
-sudo systemctl enable cec-translate ydotoold cage@tty1.service uxplay.service && \
+sudo systemctl enable cec-keyboard.service cage@tty1.service uxplay.service && \
 sudo systemctl set-default graphical.target && \
 sudo reboot
 
